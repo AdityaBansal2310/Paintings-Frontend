b63c72ed8421cd77d93cee57370a85cb5117d785
AdityaBansal2310 <=>
2024-02-12 19:20:01 +0530

    Added Border

--- src/PaintingList.js
+++ src/PaintingList.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './PaintingList.css'; // Import CSS file for styling
+import './PaintingList.css'; 
 
 function PaintingList() {
     const [paintings, setPaintings] = useState([]);
+
     useEffect(() => {
         const fetchPaintings = async () => {
             try {
                 const response = await axios.get('http://127.0.0.1:8000/painting/');
-                console.log('Paintings data:', response.data); // Log fetched data
+                console.log('Paintings data:', response.data); 
                 setPaintings(response.data);
             } catch (error) {
                 console.error('Error fetching paintings:', error);
@@ -24,13 +25,15 @@ function PaintingList() {
             <div className="painting-grid">
                 {paintings.map(painting => (
                     <div key={painting.id} className="painting-item">
-                        <h2>{painting.title}</h2>
-                        <p> ID: {painting.ID}</p>
-                        <p> Title: {painting.Title} </p>
-                        <p> Artist: {painting.Artist}</p>
-                        <p> Description: {painting.Description}</p>
-                        <p> Price: {painting.Price}</p>
-                        <img src= {painting.image_url} alt={painting.title} />
+                        <div className="painting-box">
+                            <h2>{painting.title}</h2>
+                            <p> ID: {painting.ID}</p>
+                            <p> Title: {painting.Title} </p>
+                            <p> Artist: {painting.Artist}</p>
+                            <p> Description: {painting.Description}</p>
+                            <p> Price: {painting.Price}</p>
+                            <img src={painting.image_url} alt={painting.title} />
+                        </div>
                     </div>
                 ))}
             </div>
