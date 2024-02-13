284ca538887952a896e21c5d5fccf96535e8013b
AdityaBansal2310 <=>
2024-02-13 11:53:02 +0530

    Added Post request to create new paintings

--- src/CreatePainting.js
+++ src/CreatePainting.js
@@ -4,10 +4,10 @@ import axios from 'axios';
 function CreatePainting({ history }) {
     const [paintingData, setPaintingData] = useState({
         ID: '',
-        title: '',
-        artist: '',
-        description: '',
-        price: '',
+        Title: '',
+        Artist: '',
+        Description: '',
+        Price: '',
         image_url: ''
     });
 
@@ -27,7 +27,9 @@ function CreatePainting({ history }) {
 
     const addPainting = async (paintingData) => {
         try {
-            const response = await axios.post('http://127.0.0.1:8000/painting/', paintingData);
+            const response = await axios.post('http://127.0.0.1:8000/painting/', {
+                ...paintingData
+            });
             console.log('New painting created:', response.data);
             // Here you can update the state or perform any additional actions if needed
         } catch (error) {
@@ -38,33 +40,31 @@ function CreatePainting({ history }) {
     return (
         <div>
             <h1>Create New Painting</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>ID:</label>
-                    <input type="text" name="ID" value={paintingData.ID} onChange={handleChange} />
-                </div>
-                <div>
-                    <label>Title:</label>
-                    <input type="text" name="title" value={paintingData.title} onChange={handleChange} />
-                </div>
-                <div>
-                    <label>Artist:</label>
-                    <input type="text" name="artist" value={paintingData.artist} onChange={handleChange} />
-                </div>
-                <div>
-                    <label>Description:</label>
-                    <textarea name="description" value={paintingData.description} onChange={handleChange} />
-                </div>
-                <div>
-                    <label>Price:</label>
-                    <input type="text" name="price" value={paintingData.price} onChange={handleChange} />
-                </div>
-                <div>
-                    <label>Image URL:</label>
-                    <input type="text" name="image_url" value={paintingData.image_url} onChange={handleChange} />
-                </div>
-                <button type="submit">Add Painting</button>
-            </form>
+            <div>
+                <label>ID:</label>
+                <input type="text" name="ID" value={paintingData.ID} onChange={handleChange} />
+            </div>
+            <div>
+                <label>Title:</label>
+                <input type="text" name="Title" value={paintingData.Title} onChange={handleChange} />
+            </div>
+            <div>
+                <label>Artist:</label>
+                <input type="text" name="Artist" value={paintingData.Artist} onChange={handleChange} />
+            </div>
+            <div>
+                <label>Description:</label>
+                <textarea name="Description" value={paintingData.Description} onChange={handleChange} />
+            </div>
+            <div>
+                <label>Price:</label>
+                <input type="text" name="Price" value={paintingData.Price} onChange={handleChange} />
+            </div>
+            <div>
+                <label>Image URL:</label>
+                <input type="text" name="image_url" value={paintingData.image_url} onChange={handleChange} />
+            </div>
+            <button onClick={handleSubmit}>Add Painting</button>
         </div>
     );
 }
